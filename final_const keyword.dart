95b8c134745760c839f47c4a124f2abdff6ca896
ZAMZAM ALI HEDAR <133240996+XAMZam@users.noreply.github.com>
2024-03-14 13:05:52 -0700

    DART PRACTICE

---
+++ final_const keyword.dart
@@ -0,0 +1,15 @@
+void main() {
+//final keyword
+  final cityName = 'Nairobi';
+//cityName = 'Nairobi';
+  final String countryName = 'Kenya';
+
+  //const keyword
+  const PI = 3.14;
+  const double gravity = 9.8;
+}
+
+class Circle {
+  final color = 'grey';
+  static const PI = 3.14;
+}
