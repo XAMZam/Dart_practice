7883a117bc8bf286a7765a69b55cfd74e21db455
ZAMZAM ALI HEDAR <133240996+XAMZam@users.noreply.github.com>
2024-03-14 02:33:30 -0700

    My dart practice

---
+++ data type.dart
@@ -0,0 +1,23 @@
+void main() {
+  int score = 23;
+  var count = 23; // it is infereed as interger automatically
+  int hexValue = 0xEADEBAEE;
+
+  //Numbers:double
+  double percentage = 93.4;
+  var percent = 82.5333;
+  double exponents = 1.42e5;
+
+  //strings
+  String name = 'Henry';
+  var company = 'Google';
+
+//Boolean
+  bool isValid = true;
+  var isAlive = false;
+  print(exponents);
+  print(hexValue);
+
+  //NOTE:All data types in Dart are objects.
+  //Therefore,their initial value is by default 'null'
+}
---
+++ first.dart
@@ -0,0 +1,14 @@
+void main() {
+  //This is my first line of code.
+  print('Hello World!'); //This is another commet
+
+  print('This is my first application');
+  //This is an interger value
+  print(4);
+
+  //performing arithmetic operation
+  print(12 * 4);
+
+  //printing out a boolean value
+  print(false);
+}
